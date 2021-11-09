b8d4071766758c1ddda2c4b031623a8b1d343130
Ben Arthur <arthurb@hhmi.org>
2021-11-09 08:06:54 -0500

    don't use BLAS for vector-vector ops

--- src/gpu/rls.jl
+++ src/gpu/rls.jl
@@ -3,12 +3,12 @@ function rls(k, den, e, delta, L, Ncells, r, Px, P, synInputBalanced, xtarg, lea
     batched_mul!(k, P, rtrim)
     batched_mul!(den, batched_transpose(rtrim), k)
     den .= plusone ./ (plusone .+ den)
-    batched_mul!(delta, k, den)
+    delta .= k .* den
     batched_mul!(P, k, batched_transpose(delta), minusone, plusone)
     batched_mul!(e, batched_transpose(wpWeightIn), rtrim)
     e .+= reshape(synInputBalanced .- xtarg[learn_seq,:], 1,1,Ncells)
-    batched_mul!(delta, k, e)
-    batched_mul!(delta, delta, den)
+    delta .= k .* e
+    delta .*= den
     wpWeightIn .-= delta
     wpWeightOut = convertWgtIn2Out(wpIndexIn,wpIndexConvert,wpWeightIn,wpWeightOut)
     return wpWeightIn, wpWeightOut
