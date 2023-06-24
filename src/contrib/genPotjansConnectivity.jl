d5b2e35c459597c5ca5ee739e782ef8cb731240a
Russell Jarvis <7786645+russelljjarvis@users.noreply.github.com>
2023-06-24 15:32:17 +1000

    Update genPotjansConnectivity.jl

--- src/contrib/genPotjansConnectivity.jl
+++ src/contrib/genPotjansConnectivity.jl
@@ -1,153 +1,168 @@
-using JLD2
-using Distributions, Random
-#using SparseArrays
-"""
-This file consists of a function stack that seemed necessary to achieve a network with Potjans like wiring in Julia using TrainSpikeNet.jl to simulate.
-This code draws heavily on the PyNN OSB Potjans implementation code found here:
-https://github.com/OpenSourceBrain/PotjansDiesmann2014/blob/master/PyNN/network_params.py#L139-L146
-"""
+using SparseArrays
+using StaticArrays
+using ProgressMeter
+using LinearAlgebra
 
 """
+This file contains a function stack that creates a network with Potjans and Diesmann wiring likeness in Julia using SpikingNeuralNetworks.jl to simulate
+electrical neural network dynamics.
+This code draws heavily on the PyNN OSB Potjans implementation code found here:
+https://github.com/OpenSourceBrain/PotjansDiesmann2014/blob/master/PyNN/network_params.py#L139-L146
+However the translation of this Python PyNN code into performant and scalable Julia was not trivial.
 Hard coded Potjans parameters follow.
 and then the function outputs adapted Potjans parameters.
 """
-function potjans_params(ccu, scale=1.0::Float64)
+function potjans_params(ccu)
+    # a cummulative cell count
+    #cumulative = Dict{String, Vector{Int64}}()  
+    layer_names = @SVector ["23E","23I","4E","4I","5E", "5I", "6E", "6I"] 
     # Probabilities for >=1 connection between neurons in the given populations. 
     # The first index is for the target population; the second for the source population
     #             2/3e      2/3i    4e      4i      5e      5i      6e      6i
-    conn_probs = Matrix{Float64}([0.1009  0.1689 0.0437 0.0818 0.0323 0.0     0.0076 0.    
+    conn_probs = @SMatrix [0.1009  0.1689 0.0437 0.0818 0.0323 0.0     0.0076 0.    
                                     0.1346   0.1371 0.0316 0.0515 0.0755 0.     0.0042 0.    
                                     0.0077   0.0059 0.0497 0.135  0.0067 0.0003 0.0453 0.    
                                     0.0691   0.0029 0.0794 0.1597 0.0033 0.     0.1057 0.    
                                     0.1004   0.0622 0.0505 0.0057 0.0831 0.3726 0.0204 0.    
                                     0.0548   0.0269 0.0257 0.0022 0.06   0.3158 0.0086 0.    
                                     0.0156   0.0066 0.0211 0.0166 0.0572 0.0197 0.0396 0.2252
-                                    0.0364   0.001  0.0034 0.0005 0.0277 0.008  0.0658 0.1443 ])
+                                    0.0364   0.001  0.0034 0.0005 0.0277 0.008  0.0658 0.1443 ]
 
-    layer_names = Vector{String}(["23E","23I","4E","4I","5E", "5I", "6E", "6I"])    
-    # hard coded stuff is manipulated below:
-    columns_conn_probs = [col for col in eachcol(conn_probs)][1]
-    
-    ## this list gets reorganised to reflect top excitatory bottom inhibitory.
-    ## Rearrange the whole matrix so that excitatory connections form a top partition 
-    #and inhibitory neurons form a bottom partition.
 
-    cumulative = Dict{String, Vector{Int64}}() 
-    v_old=1
-    for (k,v) in pairs(ccu)
-        ## A cummulative cell count
-        cumulative[k]=collect(v_old:v+v_old)
-        v_old=v+v_old
-    end    
-    return (cumulative,ccu,layer_names,columns_conn_probs,conn_probs)
+    # https://github.com/shimoura/ReScience-submission/blob/ShimouraR-KamijiNL-PenaRFO-CordeiroVL-CeballosCC-RomaroC-RoqueAC-2017/code/netParams.py
+    #= conn_probs = @SMatrix [0.101  0.169 0.044 0.082 0.032, 0.     0.008 0.     0.    
+                                    0.135  0.137 0.032, 0.052, 0.075, 0.,     0.004 0.     0.    
+                                    0.008  0.006 0.050, 0.135, 0.007, 0.0003, 0.045 0.     0.0983
+                                    0.069  0.003 0.079, 0.160, 0.003, 0.,     0.106 0.     0.0619
+                                    0.100  0.062 0.051, 0.006, 0.083, 0.373,  0.020 0.     0.    
+                                    0.055  0.027 0.026, 0.002, 0.060, 0.316,  0.009 0.     0.    
+                                    0.016  0.007 0.021, 0.017, 0.057, 0.020,  0.040 0.225  0.0512
+                                    0.036  0.001 0.003, 0.001, 0.028, 0.008,  0.066 0.144  0.0196 ]
+    =#
+    # hard coded network wiring parameters are manipulated below:
+
+    syn_pol = Vector{Bool}([])
+    for (i,syn) in enumerate(layer_names)
+        if occursin("E",syn) 
+            push!(syn_pol,true)
+        else
+            push!(syn_pol,false)
+        end
+    end
+    #syn_pol = syn_pol # synaptic polarity vector.
+    return (conn_probs,syn_pol)
 end
+
+
+"""
+Auxillary method, NB, this acts like the connectome constructor, so change function name to something more meaningful, like construct PotjanAndDiesmon
+A mechanism for scaling cell population sizes to suite hardware constraints.
+While Int64 might seem excessive when cell counts are between 1million to a billion Int64 is required.
+Only dealing with positive count entities so Usigned is fine.
+"""
+function potjans_constructor(scale::Float64)
+	ccu = Dict{String, UInt32}("23E"=>20683,
+		    "4E"=>21915, 
+		    "5E"=>4850, 
+		    "6E"=>14395, 
+		    "6I"=>2948, 
+		    "23I"=>5834,
+		    "5I"=>1065,
+		    "4I"=>5479)
+
+	ccu = Dict{String, UInt32}((k,ceil(Int64,v*scale)) for (k,v) in pairs(ccu))
+    v_old=1
+    K = length(keys(ccu))
+    cum_array = []# Vector{Array{UInt32}}(undef,K)
+
+    for (k,v) in pairs(ccu)
+        ## update the cummulative cell count
+        push!(cum_array,v_old:v+v_old)
+        v_old=v+v_old
+
+    end    
+
+    cum_array = SVector{8,Array{UInt32}}(cum_array) # cumulative population counts array.
+	Ncells = UInt64(sum([i for i in values(ccu)])+1)
+	Ne = UInt64(sum([ccu["23E"],ccu["4E"],ccu["5E"],ccu["6E"]]))
+    Ni = UInt64(Ncells - Ne)
+    (Ncells, Ne, Ni, ccu, cum_array)
+end
+"""
+The entry point to building the whole Potjans model in SNN.jl
+Also some of the current density parameters needed to adjust synaptic gain initial values.
+Some of the following calculations and parameters are borrowed from this repository:
+https://github.com/SpikingNetwork/TrainSpikingNet.jl/blob/master/src/param.jl
+"""
+function potjans_layer(scale::Float64)
+    (Ncells, Ne, Ni, ccu, cum_array)= potjans_constructor(scale)    
+    (conn_probs,syn_pol) = potjans_params(ccu)    
+ 
+    pree = 0.1
+    K = round(Int, Ne*pree)
+    sqrtK = sqrt(K)
+    g = 1.0
+    tau_meme = 10   # (ms)
+    je = 2.0 / sqrtK * tau_meme * g
+    ji = je#2.0 / sqrtK * tau_meme * g 
+    jee = 0.15je 
+    jei = je 
+    jie = -0.75ji 
+    jii = -ji
+    #g_strengths = Vector{Float32}([jee,jie,jei,jii])
+    Lxx = spzeros(Float32, (Ncells, Ncells))
+
+
+    cell_index_to_layer::Vector{UInt64} = zeros(Ncells)
+    build_matrix_prot!(cell_index_to_layer,jee,jei,jie,jii,Lxx,cum_array,conn_probs,syn_pol)#,g_strengths)
+    Lxx,cell_index_to_layer
+
+end
+export potjans_layer
+
+
+
+
 """
 This function contains synapse selection logic seperated from iteration logic for readability only.
 Used inside the nested iterator inside build_matrix.
 Ideally iteration could flatten to support the readability of subsequent code.
 """
-function index_assignment!(item,w0Weights,Lexc,Linh,g_strengths,Ne,Ni)  
-    (jee,jie,jei,jii) = g_strengths
-    # Mean synaptic weight for all excitatory projections except L4e->L2/3e
-    w_mean = 87.8e-3  # nA
-    w_ext = 87.8e-3  # nA
-    # Mean synaptic weight for L4e->L2/3e connections 
-    # See p. 801 of the paper, second paragraph under 'Model Parameterization', 
-    # and the caption to Supplementary Fig. 7
-    w_234 = 2 * w_mean  # nA
-    # Standard deviation of weight distribution relative to mean for L4e->L2/3e
-    # This value is not mentioned in the paper, but is chosen to match the 
-    # original code by Tobias Potjans
-    w_rel_234 = 0.05
-    # Relative inhibitory synaptic weight
-    wig = -4.
-    (src,tgt,k,k1) = item
-    if occursin("E",k) 
-        if occursin("E",k1)   
-            if occursin("4E",k) & occursin("23E",k)
-                d = Normal(w_234,w_rel_234)
-                td = truncated(d, 0.0, Inf)
-                weight = abs(rand(td, 1))
-                setindex!(w0Weights, weight, src,tgt)
-            else         
-                setindex!(w0Weights, jee, src,tgt)
-            end
-        else# meaning if the same as a logic: occursin("I",k1) is true                   
-            setindex!(w0Weights, jei, src,tgt)
-        end
-        setindex!(Lexc, w0Weights[tgt,src], src,tgt)
-        Ne=Ne+1
-    else
-        @assert occursin("I",k) 
-        # meaning meaning if the same as a logic: elseif occursin("I",k) is true  
-        if occursin("E",k1)    
-            setindex!(w0Weights, wig, src,tgt)
-        else# eaning meaning if the same as a logic: if occursin("I",k1)      is true               
-            w0Weights[tgt,src] = wig# -jii  
-            @assert occursin("I",k1) 
-        end
-        setindex!(Linh, w0Weights[tgt,src], src,tgt)
-        Ni=Ni+1
-    end        
-    (Ne,Ni)
-end
-"""
-This function contains iteration logic seperated from synapse selection logic for readability only.
-"""
-function build_matrix(cumulative::Dict{String, Vector{Int64}}, conn_probs::Matrix{Float64}, Ncells::Int32, g_strengths::Vector{Float64})    
-    edge_dict = Dict{Float32, Array}()
-    for src in 1:Ncells
-        edge_dict[src] = Int64[]
-    end    
-    #w0Weights = spzeros(Float64,Ncells,Ncells)
-    #Lexc = spzeros(Float64,Ncells,Ncells)
-    #Linh = spzeros(Float64,Ncells,Ncells)
-    w0Weights = zeros(Float64, (Ncells, Ncells))
-    Nsyne = 0 
-    Nsyni = 0
-    Nsynep = 0 
-    Nsynip = 0
-    Lexc = zeros(Float64, (Ncells, Ncells))
-    Linh = zeros(Float64, (Ncells, Ncells))
-    @inbounds for (i,(k,v)) in enumerate(pairs(cumulative))
-        @inbounds for src in v
-            @inbounds for (j,(k1,v1)) in enumerate(pairs(cumulative))
+function build_matrix_prot!(cell_index_to_layer,jee::Real,jei::Real,jie::Real,jii::Real,Lxx::SparseMatrixCSC{Float32, Int64},cum_array::SVector{8, Array{UInt32}}, conn_probs::StaticArraysCore.SMatrix{8, 8, Float64, 64}, syn_pol::Vector{Bool})#, g_strengths::Vector{Float32})
+    # excitatory weights.
+
+    @inbounds @showprogress for (i,v) in enumerate(cum_array)
+        @inbounds for (j,v1) in enumerate(cum_array)
+            prob = conn_probs[i,j]
+            @inbounds for src in v
+                cell_index_to_layer[src] = i
                 @inbounds for tgt in v1
-                    if src!=tgt                        
-                        prob = conn_probs[i,j]
+                    if src!=tgt
                         if rand()<prob
-                            append!(edge_dict[src],tgt)
-                            item = src,tgt,k,k1
-                            (Nsyne,Nsyni) = index_assignment!(item,w0Weights,Lexc,Linh,g_strengths,Nsyne,Nsyni)
+                            syn1 = syn_pol[j]
+                            syn0 = syn_pol[i]
+                            if syn0==true
+                                if syn1==true
+                                    
+                                    setindex!(Lxx,jee, src,tgt)
+                                else# meaning if the same as a logic: Inhibitory post synapse  is true                   
+                                    setindex!(Lxx,jei, src,tgt)
+
+                                end
+                            else syn0==false     
+                                if syn1==true
+
+                                    setindex!(Lxx,jie, src,tgt)
+                                else
+                                    setindex!(Lxx,jii, src,tgt)
+                                end
+                            end 
                         end
-                        @assert src!=0
-                        @assert tgt!=0
                     end
                 end
             end
         end
     end
-    return w0Weights,edge_dict,Nsyne,Nsyni,Lexc,Linh
-end
-"""
-Build the matrix from the Potjans parameters.
-###
-# The motivation for this approach is a lower memory footprint motivations.
-# a sparse matrix can be stored as a smaller dense matrix.
-# A 2D matrix should be stored as 1D matrix of srcs,tgts
-# A 2D weight matrix should be stored as 1 matrix, which is redistributed in loops using 
-# the 1D matrix of srcs,tgts.
-###
-
-"""
-function potjans_weights(args)
-    #
-    #(Ncells::Int64, jee::Float64, jie::Float64, jei::Float64, jii::Float64, ccu, scale=1.0::Float64) = args
-    Ncells, jee, jie, jei, jii, ccu, scale = args
-    (cumulative,ccu,layer_names,_,conn_probs) = potjans_params(ccu,scale)    
-    g_strengths = [jee,jie,jei,jii]
-    w0Weights,edge_dict,Ne,Ni,Lexc,Linh = build_matrix(cumulative,conn_probs,Ncells,g_strengths)
-    (edge_dict,w0Weights,Ne,Ni,Lexc,Linh)
 end
 
 
